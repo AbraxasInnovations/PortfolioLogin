b463db2d1266598cc49b3d7d31c6bda908a3e845
AbraxasInnovations <derekmogi@gmail.com>
2024-12-06 20:29:11 -0500

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,7 @@
 'use client';
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import USER_DATA from "../../data/userData";
+import USER_DATA from "../../../data/userData";
 
 export default function Login() {
   const [username, setUsername] = useState("");
