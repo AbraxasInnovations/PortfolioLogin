2fad1a3e26ba0f5eb0b50d8e028e3b6cd2862d4f
AbraxasInnovations <derekmogi@gmail.com>
2024-12-30 02:37:26 -0500

    Update userData.js

--- src/data/userData.js
+++ src/data/userData.js
@@ -22,12 +22,12 @@ const USER_DATA = {
   },
   bobbogle24: {
     name: "Robert Bogle",
-    portfolioValue: 30604, // Updated total portfolio value
+    portfolioValue: 30750, // Updated total portfolio value
     positions: [
       {
         name: "Hyperliquid Spot Strategy", // New position
         description: "Active strategy in perpetual futures",
-        value: 18604, // New position value
+        value: 18750, // New position value
         allocation: 60, // New allocation percentage
         pendingTransfer: false,
       },
